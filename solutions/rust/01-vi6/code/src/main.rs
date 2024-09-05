265d72b0f7f3d384eb8a761016be3a01e620bf40
Ryan Gang <ryan-gg@outlook.com>
2024-09-05 21:37:52 +0530

    feat: add support for rust

---
+++ solutions/rust/01-vi6/code/src/main.rs
@@ -0,0 +1,17 @@
+#![allow(unused_imports)]
+use std::net::TcpListener;
+
+fn main() {
+    let listener = TcpListener::bind("127.0.0.1:9092").unwrap();
+
+    for stream in listener.incoming() {
+        match stream {
+            Ok(_stream) => {
+                println!("accepted new connection");
+            }
+            Err(e) => {
+                println!("error: {}", e);
+            }
+        }
+    }
+}
