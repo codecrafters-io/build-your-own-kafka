31b70bc4032c92cc93f4c15685a83889df16db29
Andy Li <1450947+andy1li@users.noreply.github.com>
2024-11-05 07:27:09 +0800

    Improve based on feedback

--- solutions/cpp/01-vi6/code/src/main.cpp
+++ solutions/cpp/01-vi6/code/src/main.cpp
@@ -3,55 +3,54 @@
 #include <iostream>
 #include <netdb.h>
 #include <string>
-#include <system_error>
 #include <unistd.h>
 #include <arpa/inet.h>
 #include <sys/socket.h>
 #include <sys/types.h>
 
 int main(int argc, char* argv[]) {
-    // Flush after every std::cout / std::cerr
+    // Disable output buffering
     std::cout << std::unitbuf;
     std::cerr << std::unitbuf;
 
-    // Create socket
     int server_fd = socket(AF_INET, SOCK_STREAM, 0);
     if (server_fd < 0) {
-        throw std::system_error(errno, std::generic_category(), "Failed to create server socket");
+        std::cerr << "Failed to create server socket: " << std::endl;
+        return 1;
     }
 
-    // Set socket options
+    // Since the tester restarts your program quite often, setting SO_REUSEADDR
+    // ensures that we don't run into 'Address already in use' errors
     int reuse = 1;
     if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &reuse, sizeof(reuse)) < 0) {
         close(server_fd);
-        throw std::system_error(errno, std::generic_category(), "setsockopt failed");
+        std::cerr << "setsockopt failed: " << std::endl;
+        return 1;
     }
 
-    // Configure server address
     struct sockaddr_in server_addr{};
     server_addr.sin_family = AF_INET;
     server_addr.sin_addr.s_addr = INADDR_ANY;
     server_addr.sin_port = htons(9092);
 
-    // Bind socket
     if (bind(server_fd, reinterpret_cast<struct sockaddr*>(&server_addr), sizeof(server_addr)) != 0) {
         close(server_fd);
-        throw std::system_error(errno, std::generic_category(), "Failed to bind to port 9092");
+        std::cerr << "Failed to bind to port 9092" << std::endl;
+        return 1;
     }
 
-    // Listen for connections
     int connection_backlog = 5;
     if (listen(server_fd, connection_backlog) != 0) {
         close(server_fd);
-        throw std::system_error(errno, std::generic_category(), "listen failed");
+        std::cerr << "listen failed" << std::endl;
+        return 1;
     }
 
     std::cout << "Waiting for a client to connect...\n";
 
-    // Accept client connection
     struct sockaddr_in client_addr{};
     socklen_t client_addr_len = sizeof(client_addr);
-    
+
     int client_fd = accept(server_fd, reinterpret_cast<struct sockaddr*>(&client_addr), &client_addr_len);
     std::cout << "Client connected\n";
     close(client_fd);
